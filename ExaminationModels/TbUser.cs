b680b717a3c4d882a1b29b2fdae59f04c36ec9b3
BigGoodLoong <chengrongchn@163.com>
2017-05-11 21:05:40 +0800

    last three class!

---
+++ ExaminationModels/TbUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationModels
+{
+    public class TbUser
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int YhID { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string YhName { get; set; }
+
+        /// <summary>
+        /// 学号 
+        /// </summary>
+        public string Xh { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string YhPwd { get; set; }
+
+        /// <summary>
+        /// 用户身份(1为管理员，2为老师，3为学生)
+        /// </summary>
+        public int Zt { get; set; }
+    }
+}
