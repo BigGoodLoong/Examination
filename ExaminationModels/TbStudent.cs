9f5bd2d17b3e8ecb4763d393a70ad3777a4815d2
BigGoodLoong <chengrongchn@163.com>
2017-05-11 20:49:26 +0800

    add TbStudent and TbSubject.

---
+++ ExaminationModels/TbStudent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationModels
+{
+    public class TbStudent
+    {
+        /// <summary>
+        /// 学生ID
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        public string XsName { get; set; }
+        /// <summary>
+        /// 学生性别
+        /// </summary>
+        public string XsSex { get; set; }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int YhID { get; set; }
+        /// <summary>
+        /// 班级名称
+        /// </summary>
+        public string BjName { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
---
+++ ExaminationModels/TbSubject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationModels
+{
+    public class TbSubject
+    {
+        /// <summary>
+        /// 科目ID
+        /// </summary>
+        public int KmID { get; set; }
+        /// <summary>
+        /// 科目名称
+        /// </summary>
+        public string KmName { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+        /// <summary>
+        /// 专业ID
+        /// </summary>
+        public int ZyID { get; set; }
+    }
+}
