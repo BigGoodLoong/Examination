5f142d1c8c8c8c70b6d90219431872094567d0fa
BigGoodLoong <chengrongchn@163.com>
2017-05-23 14:33:11 +0800

    add Login.aspx...etc and images

--- ExaminationModels/TbTestPaper.cs
+++ ExaminationModels/TbTestPaper.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace ExaminationModels
 {
-    class TbTestPaper
+    /// <summary>
+    /// 试卷类
+    /// </summary>
+    public class TbTestPaper
     {
+        public int SjID { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string LsName { get; set; }
+        /// <summary>
+        /// 试卷名称
+        /// </summary>
+        public string SjName { get; set; }
+        /// <summary>
+        /// 科目ID
+        /// </summary>
+        public int KmID { get; set; }
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime KsTime { get; set; }
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime JsTime { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CjTime { get; set; }
+        /// <summary>
+        /// 试卷状态，1为没有标准答案，2为有标准答案
+        /// </summary>
+        public int Zt { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+
     }
 }
---
+++ ExaminationUI/ajax.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ajax.aspx.cs" Inherits="ajax" %>
+
+
