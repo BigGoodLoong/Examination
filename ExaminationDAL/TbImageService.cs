cf453d789f0d29cdf131d1c3533b93f16259e133
zhuyangyang74 <782376804@qq.com>
2017-05-25 00:11:46 +0800

    Add TbImageService.cs and TbObjectiveTopicService.cs

---
+++ ExaminationDAL/TbImageService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExaminationModels;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ExaminationDAL
+{
+    public class TbImageService
+    {
+        /// <summary>
+        /// 添加试卷图片
+        /// </summary>
+        /// <param name="image"></param>
+        public void AddImage(TbImages image)
+        {
+            string sql = "insert into tbImages(SjID,TpYm,Tpian)values(@SjID,@TpYm,@Tpian)";
+            SqlParameter[] paras = new SqlParameter[] {
+                    new SqlParameter("@SjID",image.SjID),
+                    new SqlParameter("@TpYm",image.TpYm),
+                    new SqlParameter("@Tpian",image.Tpian)
+            };
+            DBHelper.ExecuteCommand(sql, paras);
+        }
+        /// <summary>
+        /// 根据试卷ID获取对应所有图片
+        /// </summary>
+        /// <param name="sjid">试卷ID</param>
+        /// <returns></returns>
+        public DataTable GetImagesBySjid(int sjid)
+        {
+            string sql = "select TpYm,Tpian  from tbImages where SjID=@SjID order by TpYm asc";
+            SqlParameter[] paras = new SqlParameter[] {
+                    new SqlParameter("@SjID",sjid)
+            };
+            return DBHelper.GetDataTable(sql, paras);
+        }
+        /// <summary>
+        /// 根据试卷ID删除对应所有图片
+        /// </summary>
+        /// <param name="sjid">试卷ID</param>
+        /// <returns></returns>
+        public int DeleteImageBySjid(int sjid)
+        {
+            string sql = "delete from tbImages where SjID=@SjID";
+            SqlParameter[] paras = new SqlParameter[] {
+                    new SqlParameter("@SjID",sjid)
+            };
+            return DBHelper.ExecuteCommand(sql, paras);
+        }
+    }
+}
---
+++ ExaminationDAL/TbObjectiveTopicService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+//using ExaminationModels;
+using System.Data.SqlClient;
+
+namespace ExaminationDAL
+{
+    public class TbObjectiveTopicService
+    {
+        /// <summary>
+        /// 查询学员答卷列表信息
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetAllReviewTestPaperInfo()
+        {
+            StringBuilder sqlsb = new StringBuilder();
+            sqlsb.Append("select ot.KgtID,u.Xh, tp.SjName,sl.ZyName, sb.KmName ,ot.Zt ");
+            sqlsb.Append("from tbObjTopic as ot,tbTestPaper as tp,tbSubject as sb,tbUser as u,tbSpeciality as sl ");
+            sqlsb.Append("where tp.SjID=ot.SjID and tp.KmID=sb.KmID and ot.YhID=u.YhID and sb.ZyID=sl.ZyID and ot.Zt<>1 ");
+            return DBHelper.GetDataTable(sqlsb.ToString());
+        }
+        /// <summary>
+        /// 查询所有学员答卷列表信息的条数
+        /// </summary>
+        /// <returns></returns>
+        public int GetATestPaperCount()
+        {
+            StringBuilder sqlSb = new StringBuilder();
+            sqlSb.Append("select COUNT(*)from tbObjTopic as ot,tbTestPaper as tp,tbSubject as sb,tbUser as u,tbSpeciality as sl ");
+            sqlSb.Append("where tp.SjID=ot.SjID and tp.KmID=sb.KmID and ot.YhID=u.YhID and sb.ZyID=sl.ZyID and ot.Zt<>1 ");
+            return DBHelper.GetScalar(sqlSb.ToString());
+        }
+        /// <summary>
+        /// 根据答题卡ID删除对应答题卡信息
+        /// </summary>
+        /// <param name="KgtID">答题卡ID</param>
+        /// <returns></returns>
+        public int deleteReviewTestPaperInfo(int KgtID)
+        {
+            string sql = "delete from dbo.tbObjTopic where KgtID=@KgtID";
+            SqlParameter[] paras = new SqlParameter[] {
+                    new SqlParameter("@KgtID",KgtID)
+            };
+            return DBHelper.ExecuteCommand(sql, paras);
+        }
+    }
+}
