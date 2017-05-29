54964f845b5ce787ab0963982cd1ac09e5745be5
WYZ <704015504@qq.com>
2017-05-29 09:47:06 +0800

    add classes

---
+++ ExaminationBLL/TbTeacherManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExaminationDAL;
+using ExaminationModels;
+using System.Data.SqlClient;
+using System.Data;
+namespace ExaminationBLL
+{
+    public class TbTeacherManager
+    {
+        static TbTeacherService tbteacherservice = new TbTeacherService();
+
+
+        /// <summary>
+        /// 新增教师信息
+        /// </summary>
+        /// <param name="user">账号</param>
+        /// <returns></returns>
+        public static int CheckTeacherUser(string user)
+        {
+            return tbteacherservice.CheckTeacherUser(user);
+        }
+        /// <summary>
+        /// 将教师的用户名和密码添加到用户表里
+        /// </summary>
+        /// <param name="user">账号</param>
+        /// <param name="xh">学号</param>
+        /// <param name="pwd">密码</param>
+        /// <param name="zt">状态</param>
+        /// <returns></returns>
+        public static int InsertTeacherUser(string user, string xh, string pwd, int zt)
+        {
+            return tbteacherservice.InsertTeacherUser(user, xh, pwd, zt);
+        }
+
+        /// <summary>
+        /// 获取教师所教专业的专业ID
+        /// </summary>
+        /// <param name="zy">专业</param>
+        /// <returns></returns>
+        public static int GetTeacherZyID(string zy)
+        {
+            return tbteacherservice.GetTeacherZyID(zy);
+        }
+        /// <summary>
+        /// 新增教师信息
+        /// </summary>
+        /// <param name="yhid">用户ID</param>
+        /// <param name="name">教师姓名</param>
+        /// <param name="zyid">专业ID</param>
+        /// <param name="remark">备注</param>
+        /// <returns></returns>
+        public static int InsertTeacherInfo(int yhid, string name, int zyid, string remark)
+        {
+            return tbteacherservice.InsertTeacherInfo(yhid, name, zyid, remark);
+        }
+        /// <summary>
+        /// 返回当前教师列表信息的记录总数
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable GetAllTeachersInfo(string lsname)
+        {
+            return tbteacherservice.GetAllTeachersInfo(lsname);
+        }
+
+        /// <summary>
+        /// 返回当前教师列表信息
+        /// </summary>
+        /// <param name="len">每页显示条数</param>
+        /// <param name="page">当前页数</param>
+        /// <returns></returns>
+        public static DataTable GetAllTeacherInfoPage(int len, int page, string lsname)
+        {
+            return tbteacherservice.GetAllTeacherInfoPage(len, page, lsname);
+        }
+        /// <summary>
+        /// 根据专业ID删除对应老师信息
+        /// </summary>
+        /// <param name="ZyID">专业ID</param>
+        /// <returns></returns>
+        public static int DeleteTeaByZyID(int ZyID)
+        {
+            return tbteacherservice.DeleteTeaByZyID(ZyID);
+        }
+        /// </summary>
+        /// <param name="LsID">教师编号</param>
+        /// <returns></returns>
+        public static int DeleteTeacherInfo(int YhID)
+        {
+            return tbteacherservice.DeleteTeacherInfo(YhID);
+        }
+        /// <summary>
+        /// 根据教师的用户编号获取教师的详细信息
+        /// </summary>
+        /// <param name="YhID"></param>
+        /// <returns></returns>
+        public static TbTeacher GetTeacherInfoByLsID(int YhID)
+        {
+            return tbteacherservice.GetTeacherInfoByLsID(YhID);
+        }
+        /// <summary>
+        /// 修改教师信息
+        /// </summary>
+        /// <param name="teacher">修改后的教师信息</param>
+        /// <returns></returns>
+        public static int UpdateTeacherInfoByYhID(TbTeacher teacher)
+        {
+            return tbteacherservice.UpdateTeacherInfoByYhID(teacher);
+        }
+    }
+}
---
+++ ExaminationBLL/TbTestPaperManager.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExaminationModels;
+using ExaminationDAL;
+using System.Data;
+
+namespace ExaminationBLL
+{
+    public class TbTestPaperManager
+    {
+        static TbTestPaperService paperService = new TbTestPaperService();
+
+        /// <summary>
+        /// 添加一张试卷，返回当前试卷ID
+        /// </summary>
+        /// <param name="paper"></param>
+        /// <returns></returns>
+        public static int AddPaper(TbTestPaper paper)
+        {
+            return paperService.AddPaper(paper);
+        }
+
+        /// <summary>
+        /// 查询试卷列表
+        /// </summary>
+        /// <param name="len">每次查询的条数</param>
+        /// <param name="page">当前页数</param>
+        /// <returns></returns>
+        public static DataTable GetAllTestpaperByPage(int len, int page)
+        {
+            return paperService.GetAllTestpaperByPage(len, page);
+        }
+        public static DataTable GetAllTestpaperByPages(int len, int page, string sjName)
+        {
+            return paperService.GetAllTestpaperByPage(len, page, sjName);
+        }
+        public static DataTable GetAllTestpaperByPages(int len, int page, string sjName, int OK)
+        {
+            return paperService.GetAllTestpaperByPage(len, page, sjName, OK);
+        }
+
+        /// <summary>
+        /// 根据ID删除试卷信息
+        /// </summary>
+        /// <param name="sjid">试卷ID</param>
+        /// <returns></returns>
+        public static int DeleteTestpaperBySjid(int sjid)
+        {
+            return paperService.DeleteTestpaperBySjid(sjid);
+        }
+        /// <summary>
+        /// 查询试卷信息的总条数
+        /// </summary>
+        /// <returns></returns>
+        public static int GetAllTestpaperCount()
+        {
+            return paperService.GetAllTestpaperCount();
+        }
+        /// <summary>
+        /// 根据科目ID查找试卷信息
+        /// </summary>
+        /// <param name="KmID">科目ID</param>
+        /// <returns></returns>
+        public static int GetAllTestPaperIsExist(int KmID)
+        {
+            return paperService.GetAllTestPaperIsExist(KmID);
+        }
+
+        /// <summary>
+        /// 修改试卷状态 用户增加答案和删除答案
+        /// </summary>
+        /// <param name="Sjid">试卷ID</param>
+        /// <param name="Zt">试卷状态</param>
+        /// <returns></returns>
+        public static int UpPaperZt(string Sjid, int Zt)
+        {
+            return paperService.UpPaperZt(Sjid, Zt);
+        }
+        /// <summary>
+        /// 查询所有试卷列表信息
+        /// </summary>
+        /// <param name="len">每页显示的列数</param>
+        /// <param name="Page">页号</param>
+        /// <returns></returns>
+        public static DataTable GetAllTestPaperInfo(int len, int Page)
+        {
+            return paperService.GetAllTestPaperInfo(len, Page);
+        }
+
+        /// <summary>
+        /// 查询试卷列表
+        /// </summary>
+        /// <param name="len">每页显示信息的条数</param>
+        /// <param name="page">页号</param>
+        /// <param name="testpaperName">查找条件</param>
+        /// <returns></returns>
+        public static DataTable GetAllTestpaperByPage(int len, int page, string testpaperName)
+        {
+            return paperService.GetAllTestpaperByPage(len, page, testpaperName);
+        }
+        /// <summary>
+        /// 查询试卷列表信息的总条数
+        /// </summary>
+        /// <returns></returns>
+        public static int GetAllTestPaperInfoCount()
+        {
+            return paperService.GetAllTestpaperCount();
+        }
+        public static DataTable GetAllTestpaperCount(string testpaper)
+        {
+            return paperService.GetAllTestpaperCount(testpaper);
+        }
+        /// <summary>
+        /// 根据试卷ID获得一张试卷的全部信息
+        /// </summary>
+        /// <param name="SjID">试卷ID</param>
+        /// <returns></returns>
+        public static DataTable getOnePaperInfo(string SjID)
+        {
+            return paperService.getOnePaperInfo(SjID);
+        }
+        /// <summary>
+        /// 根据试卷ID查询其对应详细信息
+        /// </summary>
+        /// <param name="sjid">试卷ID</param>
+        /// <returns></returns>
+        public static TbTestPaper GetAllSjInfo(int sjid)
+        {
+            return paperService.GetAllSjInfo(sjid);
+        }
+        /// <summary>
+        /// 根据试卷名判断是否存在对应的试卷
+        /// </summary>
+        /// <param name="Sjname">试卷名</param>
+        /// <returns></returns>
+        public static int GetTestPaperInfo(string Sjname)
+        {
+            return paperService.GetTestPaperInfo(Sjname);
+        }
+        /// <summary>
+        /// 根据试卷ID修改试卷信息
+        /// </summary>
+        /// <param name="SjName">试卷名</param>
+        /// <param name="startTime">开始时间</param>
+        /// <param name="endTime">结束时间</param>
+        /// <param name="remark">备注</param>
+        /// <param name="SjID">试卷ID</param>
+        /// <returns></returns>
+        public static int updateTestPaper(string SjName, DateTime startTime, DateTime endTime, string remark, int SjID)
+        {
+            return paperService.updateTestPaper(SjName, startTime, endTime, remark, SjID);
+        }
+    }
+}
